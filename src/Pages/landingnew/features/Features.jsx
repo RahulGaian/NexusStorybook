58c2c415f26b8424c8634f9103f268bad9247c4a
hussainhafidh <hussainhafidh1@gmail.com>
2023-11-20 10:20:31 +0530

    built some components of landing page

---
+++ src/Pages/landingnew/features/Features.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "./Features.css";
+
+const Features = () => {
+  return (
+    <div>
+      <div className="parent_features">
+        <div className="productContentCardWrapper">
+          <div className="productContentCard ">
+            <div className="productContentCardImage">
+              <img
+                src="/images/imagesnew/Nexus_connect/landingpage/comp-wifi.png"
+                alt="comp-wifi img"
+              />
+            </div>
+            <div className="productContentCardDesc">
+              <h1>Discover a World of NextGenTV Experiences</h1>
+              <p>
+                With NextGenTV Marketplace, viewers can explore a vast array of
+                innovative applications tailored for the NextGenTV platform.
+                From interactive content and enhanced viewing experiences to
+                personalized recommendations and immersive storytelling, our
+                marketplace brings cutting-edge applications at your fingertips.{" "}
+              </p>
+            </div>
+          </div>
+
+          <div className="productContentCard ">
+            <div className="productContentCardImage">
+              <img
+                src="/images/imagesnew/Nexus_connect/landingpage/comp-tower.png"
+                alt="comp-tower img"
+              />
+            </div>
+            <div className="productContentCardDesc">
+              <h1>Monetize Your NextGenTV Offerings</h1>
+              <p>
+                At NextGenTV Marketplace, we believe in empowering content
+                creators and application developers to monetize their offerings.
+                Our platform offers a variety of monetization models, including
+                advertising, subscriptions, and in-app purchases, giving you the
+                flexibility to choose the approach that aligns with your
+                business goals. Benefit from our revenue-sharing mechanism and
+                tap into the growing market of NextGenTV viewers to generate
+                revenue from your innovative applications.
+              </p>
+            </div>
+          </div>
+
+          <div className="productContentCard ">
+            <div className="productContentCardImage">
+              <img
+                src="images/imagesnew/Nexus_connect/landingpage/computer-img.png"
+                alt="computer-img"
+              />
+            </div>
+            <div className="productContentCardDesc">
+              <h1>Drive User Engagement and Optimize Performance</h1>
+              <p>
+                With NextGenTV Marketplace, viewers can explore a vast array of
+                innovative applications tailored for the NextGenTV platform.
+                From interactive content and enhanced viewing experiences to
+                personalized recommendations and immersive storytelling, our
+                marketplace brings cutting-edge applications at your fingertips.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
