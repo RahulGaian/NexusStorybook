91d381f92bf7ff55f499b5388076ba4910ebcb08
hussainhafidh <hussainhafidh1@gmail.com>
2023-11-20 13:56:16 +0530

    updated landing page components

--- src/Pages/landingnew/index.jsx
+++ src/Pages/landingnew/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./landing.css";
 // import AppSwiper from "../../components/appSwiper";
 // import AndroidApps from "../../components/AndroidAppsSwiper";
-import Nexus from "./map_component/components/comp";
+// import Nexus from "./map_component/components/comp";
 import videofile from "./video/people-connect-digital-wave-2022-08-05-01-43-14-utc.mp4";
 import Features from "./features/Features";
 
@@ -165,9 +165,9 @@ const Landingpagenew = () => {
           </div>
         </div>
 
-        <div>
+        {/* <div>
           <Nexus />
-        </div>
+        </div> */}
 
 
         {/* <div><AppSwiper /></div> */}
