fcaccec2f12828c2907909ffaec522bde67ae628
Yogesh K U <yogesh.ku@gaiansolutions.com>
2023-12-15 17:43:47 +0530

    cookies-component-updated

--- src/molecules/cookies/index.jsx
+++ src/molecules/cookies/index.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./index.module.css";
+import { cookiesData } from "./cookiesData";
 
 function Cookies() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const toggleAccordion = (index) => {
+    setActiveIndex(activeIndex === index ? 0 : index);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((activeIndex) => activeIndex);
+    });
+  });
   return (
     <div className={style.cookies}>
       <div className={style.cookiesContainer}>
@@ -27,7 +39,7 @@ function Cookies() {
         </div>
         <div className={style.cookiesFooterContainer}>
           <div className={style.cookiesFooter}>
-            <h6>SwissBorg app</h6>
+            <h6 className={style.cookiesFooterHeader}>SwissBorg app</h6>
             <div className={style.cookiesFooterList}>
               <p>Buy cryptos</p>
               <p>Earn cryptos</p>
@@ -70,6 +82,148 @@ function Cookies() {
               </p>
               <p>Affiliate program</p>
               <p>Help Center</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* mobile view */}
+      <div className={style.mobileCookiesContainer}>
+        <div className={style.mobileCookiesHeader}>
+          <div className={style.mobileCookiesLeftHeader}>
+            <img
+              src="./Images/cookies/cookies-icon.png"
+              alt="cookies-icon"
+              className={style.mobileCookiesLeftImage}
+            />
+            <p className={style.mobileCookiesLeftSubheading}>
+              Manage cookies settings
+            </p>
+          </div>
+          <div className={style.mobileCookiesRightHeader}>
+            <p>English</p>
+            <img
+              src="./Images/cookies/cookies-arrow.png"
+              alt="cookies-icon"
+              className={style.mobileCookiesRightImage}
+            />
+          </div>
+        </div>
+        {/* Accordion Component */}
+        <div className={style.mobileCookiesAccordionCont}>
+          {/* 1st Accordion */}
+          <div
+            className={style.mobileCookiesAccordion}
+            onClick={() => {
+              toggleAccordion(1);
+            }}
+          >
+            <div className={style.mobileCookiesAccordionHeaderCont}>
+              <div className={style.mobileCookiesAccordionHeader}>
+                {cookiesData[0].header}
+              </div>
+              <img
+                src="/Images/cookies/dropdown.png"
+                alt="dropdown"
+                className={style.mobileCookiesAccordionImg}
+              />
+            </div>
+            <div
+              className={`${style.mobileCookiesAccordionContent} ${
+                activeIndex === 1 ? style.active : ""
+              }`}
+            >
+              <div className={style.mobileCookiesAccordionList}>
+                {cookiesData[0].sections[0].items.map((item, index) => (
+                  <div key={index}>{item}</div>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/* 2nd Accordion */}
+          <div
+            className={style.mobileCookiesAccordion}
+            onClick={() => {
+              toggleAccordion(2);
+            }}
+          >
+            <div className={style.mobileCookiesAccordionHeaderCont}>
+              <div className={style.mobileCookiesAccordionHeader}>
+                {cookiesData[1].header}
+              </div>
+              <img
+                src="/Images/cookies/dropdown.png"
+                alt="dropdown"
+                className={style.mobileCookiesAccordionImg}
+              />
+            </div>
+            <div
+              className={`${style.mobileCookiesAccordionContent} ${
+                activeIndex === 2 ? style.active : ""
+              }`}
+            >
+              <div className={style.mobileCookiesAccordionList}>
+                {cookiesData[1].sections[0].items.map((item, index) => (
+                  <div key={index}>{item}</div>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/* 3rd Accordion */}
+          <div
+            className={style.mobileCookiesAccordion}
+            onClick={() => {
+              toggleAccordion(3);
+            }}
+          >
+            <div className={style.mobileCookiesAccordionHeaderCont}>
+              <div className={style.mobileCookiesAccordionHeader}>
+                {cookiesData[2].header}
+              </div>
+              <img
+                src="/Images/cookies/dropdown.png"
+                alt="dropdown"
+                className={style.mobileCookiesAccordionImg}
+              />
+            </div>
+            <div
+              className={`${style.mobileCookiesAccordionContent} ${
+                activeIndex === 3 ? style.active : ""
+              }`}
+            >
+              <div className={style.mobileCookiesAccordionList}>
+                {cookiesData[2].sections[0].items.map((item, index) => (
+                  <div key={index}>{item}</div>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/* 4th Accordion */}
+          <div
+            className={style.mobileCookiesAccordion}
+            onClick={() => {
+              toggleAccordion(4);
+            }}
+          >
+            <div className={style.mobileCookiesAccordionHeaderCont}>
+              <div className={style.mobileCookiesAccordionHeader}>
+                {cookiesData[3].header}
+              </div>
+              <img
+                src="/Images/cookies/dropdown.png"
+                alt="dropdown"
+                className={style.mobileCookiesAccordionImg}
+              />
+            </div>
+            <div
+              className={`${style.mobileCookiesAccordionContent} ${
+                activeIndex === 4 ? style.active : ""
+              }`}
+            >
+              <div className={style.mobileCookiesAccordionList}>
+                {cookiesData[3].sections[0].items.map((item, index) => (
+                  <div key={index}>{item}</div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
