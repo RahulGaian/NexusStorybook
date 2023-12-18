fd2f9985e6502ca128d423fd5d7d1aee8c5a7481
Yogesh K U <yogesh.ku@gaiansolutions.com>
2023-12-18 18:28:52 +0530

    explore-thematics-updated

---
+++ src/molecules/investmentData/investmentData.js
@@ -0,0 +1,16 @@
+export const InvestmentData = {
+  investmentOne: [
+    {
+      investors1: "17,366",
+      investorsName1: "Total Thematics Investors",
+    },
+    {
+      investors2: "51,5555",
+      investorsName2: "Total Investors",
+    },
+    {
+      investors3: "41,000",
+      investorsName3: "Total Thematics",
+    },
+  ],
+};
