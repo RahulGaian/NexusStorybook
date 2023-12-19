9a53f936736d4cb2b14952c58a54f992465ecd40
ShubhamLahase <shubham.sheshrao@gaiansolutions.com>
2023-12-19 18:40:30 +0530

    infographic card component done

---
+++ src/molecules/infographic_card/data.js
@@ -0,0 +1,37 @@
+export const ifgData = {
+  c1: {
+    imgURL: "./Images/infographicCards/thi1.png",
+    head: "Diversify risk in one tap",
+    content:
+      "95% of crypto projects fail. Putting all your bets on one token is highly risky. Instead broaden your horizons and invest across your favourite trends & themes, all with just one tap.",
+    flexDirection: "row",
+  },
+  c2: {
+    imgURL: "./Images/infographicCards/thi2.png",
+    head: "Grow with expert selections",
+    content:
+      "SwissBorg meticulously curates each Thematic. Every token included is thoroughly vetted and consistently reviewed. We make sure you're invested in each theme's top projects.",
+    flexDirection: "row-reverse",
+  },
+  c3: {
+    imgURL: "./Images/infographicCards/thw1.png",
+    head: "Invest in the top projects only",
+    content:
+      "Thematics isn't about following the crowd. Our expert team conducts thorough evaluations of each project ensuring you invest in the most promising tokens in each theme.",
+    flexDirection: "row",
+  },
+  c4: {
+    imgURL: "./Images/infographicCards/thw2.png",
+    head: "Rebalancing to keep your investment balanced",
+    content:
+      "Thematics automatically adjust your investments to maintain the best mix of assets. As market conditions change, Rebalancing looks after your risk management. It takes profit from what's performing the best and buys more of what hasn't risen as much, keeping your portfolio in tip-top shape.",
+    flexDirection: "row-reverse",
+  },
+  c5: {
+    imgURL: "./Images/infographicCards/core.png",
+    head: "The simple way to manage your risk appetite",
+    content:
+      "Every strategy for every crypto asset has a Cyborg mood (Core, Adventurous, Brave) that grades its risks so you can make smart investing decisions.",
+    flexDirection: "row",
+  }
+};
