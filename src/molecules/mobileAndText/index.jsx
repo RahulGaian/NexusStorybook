aa5ae5bd7948caaca25a445c1433cf61bbad308a
Yogesh K U <yogesh.ku@gaiansolutions.com>
2023-12-20 18:53:15 +0530

    mobileandtext-newsletter-components-updated

--- src/molecules/mobileAndText/index.jsx
+++ src/molecules/mobileAndText/index.jsx
@@ -1,34 +1,34 @@
-// import React from "react";
-// import style from "./index.module.css";
+import React from "react";
+import style from "./index.module.css";
 
-// function MobileAndText({ data }) {
-//   const { mobileText, mobileImage } = data;
-//   console.log(data);
+function MobileAndText({ data }) {
+  const { heading, description, images } = data;
 
-//   return (
-//     <div className={style.mobileContainer}>
-//       <div className={style.mobileContentContainer}>
-//         <div className={style.mobileContentLeft}>
-//           <div className={style.mobileLeftHeader}>
-//             <p>{mobileText.heading}</p>
-//           </div>
-//           <div className={style.mobileLeftDesc}>
-//             <p>{mobileText.desc1}</p>
-//             <p>{mobileText.desc2}</p>
-//             <p>{mobileText.desc3}</p>
-//           </div>
-//           <div className={style.mobileLeftImage}>
-//             <img src={mobileText.img1} alt="" />
-//             <img src={mobileText.img2} alt="" />
-//             <img src={mobileText.img3} alt="" />
-//           </div>
-//         </div>
-//         <div className={style.mobileContentRight}>
-//           <img src={mobileImage.mobImg} alt="" />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+  return (
+    <div className={style.mobileContainer}>
+      <div className={style.mobileContentContainer}>
+        <div className={style.mobileContentLeft}>
+          <div className={style.mobileLeftHeader}>
+            <p>{heading}</p>
+          </div>
+          <div className={style.mobileLeftDesc}>
+            {Object.values(description).map((desc, index) => (
+              <p key={index}>{desc}</p>
+            ))}
+          </div>
+          <div className={style.mobileLeftImage}>
+            {Object.values(images).map((image, index) => (
+              <img key={index} src={image} alt="" />
+            ))}
+          </div>
+        </div>
+        {/* Assuming mobImg is directly in the data object */}
+        <div className={style.mobileContentRight}>
+          <img src={data.mobImg} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
 
-// export default MobileAndText;
+export default MobileAndText;
--- src/molecules/mobileAndText/mobileAndTextData.js
+++ src/molecules/mobileAndText/mobileAndTextData.js
@@ -2,48 +2,55 @@ export const mobileAndTextData = {
   mobileData1: {
     mobileText1: {
       heading: "Diversify like the pros injust 3 steps. Start now!",
-      desc1:
-        " In the Invest tab in the SwissBorg app look for the Thematicsproduct.",
-      desc2: "Head to the Thematics overview screen and choose your Thematic.",
-      desc3:
-        "Invest your desired amount & watch the full power of Thematics strengthen your portfolio.",
-      img1: "./Images/mobileAndText/appstore.png",
-      img2: "./Images/mobileAndText/googlestore.png",
-      img3: "./Images/mobileAndText/scanner.png",
-    },
-    mobileImage1: {
+      description: {
+        desc1:
+          " In the Invest tab in the SwissBorg app look for the Thematics product.",
+        desc2:
+          "Head to the Thematics overview screen and choose your Thematic.",
+        desc3:
+          "Invest your desired amount & watch the full power of Thematics strengthen your portfolio.",
+      },
+      images: {
+        img1: "./Images/mobileAndText/appstore.png",
+        img2: "./Images/mobileAndText/googlestore.png",
+        img3: "./Images/mobileAndText/scanner.png",
+      },
       mobImg: "./Images/mobileAndText/mobileimage.png",
     },
   },
   mobileData2: {
     mobileText2: {
       heading: "Use your card, add funds,and start investing in minutes!",
-      desc1: " Tap Deposit button on the main screen",
-      desc2: "Choose add card",
-      desc3: "Provide card details",
-      desc4: "Select the currency and enter amount",
-      desc5: "Authenticate payment with 3D Secure",
-      img1: "./Images/mobileAndText/appstore.png",
-      img2: "./Images/mobileAndText/googlestore.png",
-      img3: "./Images/mobileAndText/scanner.png",
-    },
-    mobileImage2: {
+      description: {
+        desc1: " Tap Deposit button on the main screen",
+        desc2: "Choose add card",
+        desc3: "Provide card details",
+        desc4: "Select the currency and enter amount",
+        desc5: "Authenticate payment with 3D Secure",
+      },
+      images: {
+        img1: "./Images/mobileAndText/appstore.png",
+        img2: "./Images/mobileAndText/googlestore.png",
+        img3: "./Images/mobileAndText/scanner.png",
+      },
       mobImg: "./Images/mobileAndText/mobileimage1.png",
     },
   },
   mobileData3: {
     mobileText3: {
       heading: "Start earning now with SwissBorg Earn",
-      desc1: " Choose the asset and strategy that suits you.",
-      desc2:
-        "Enter the amount and accept the terms of use by swiping the button.",
-      desc3:
-        "Check to see you’ve entered the correct sum, click top up and start earning.",
-      img1: "./Images/mobileAndText/appstore.png",
-      img2: "./Images/mobileAndText/googlestore.png",
-      img3: "./Images/mobileAndText/scanner.png",
-    },
-    mobileImage3: {
+      description: {
+        desc1: " Choose the asset and strategy that suits you.",
+        desc2:
+          "Enter the amount and accept the terms of use by swiping the button.",
+        desc3:
+          "Check to see you’ve entered the correct sum, click top up and start earning.",
+      },
+      images: {
+        img1: "./Images/mobileAndText/appstore.png",
+        img2: "./Images/mobileAndText/googlestore.png",
+        img3: "./Images/mobileAndText/scanner.png",
+      },
       mobImg: "./Images/mobileAndText/mobileimage2.png",
     },
   },
