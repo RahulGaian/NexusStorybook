13c638afb3fbacdaa3bbb5019a0efef382d3498e
hussainhafidh <hussainhafidh1@gmail.com>
2023-12-21 18:40:19 +0530

    buily dynamic accordian

--- src/molecules/dynamicAccordian/data.js
+++ src/molecules/dynamicAccordian/data.js
@@ -29,5 +29,47 @@ export const dynamicAccordianData = {
     ],
   },
 
+  dynamicAccordian2: {
+    dropdownsData: [
+      {
+        title:"Is Auto-Invest suitable for any market condition?",
+        content: "Thematic investing is an investment approach that focuses on capitalizing on long-term trends and specific themes, rather than traditional asset classes, to build a diversified portfolio.",
+      },
+      {
+        title:"Can I use my bank account with Auto-Invest?",
+        content:"Rebalancing in thematic investing involves adjusting the allocation of assets within a portfolio to maintain the desired exposure to specific themes or trends.",
+
+      },
+      {
+        title:"Are Auto-Invest fees on top of exchange fees?",
+        content: "For your portfolio to remain relevant & well diversified in fast-changing markets",
+      },
+      {
+        title:"When is Auto-Invest executing the exchange?",
+        content: "Yes, with Thematics you own all the underlying assets. It is a bundle of tokens, contrary to an ETF or an  index",
+      },
+      {
+        title:"Can I Auto-Invest into Thematics?",
+        content: "The minimum investment amount for the Golden Thematics is 40 USD or the equivalent in your local currency",
+      },
+      {
+        title:"Can I use several tokens to invest in 1 token when using Auto-Invest?",
+        content: "There is no minimum investment period. You are free to sell your Thematic investment at any time. In the future, you will be able to earn rewards based on your holding duration.",
+      },
+      {
+        title:"Can I Auto-Invest from my Earn account?",
+        content: "There is no minimum investment period. You are free to sell your Thematic investment at any time. In the future, you will be able to earn rewards based on your holding duration.",
+      },
+      {
+        title:"Where can I see the Auto-Invest transaction?",
+        content: "There is no minimum investment period. You are free to sell your Thematic investment at any time. In the future, you will be able to earn rewards based on your holding duration.",
+      },
+      {
+        title:"What’s next for Auto-Invest?",
+        content: "There is no minimum investment period. You are free to sell your Thematic investment at any time. In the future, you will be able to earn rewards based on your holding duration.",
+      },
+    ],
+  },
+
 };
 
