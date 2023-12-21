c6ae67a421aa7949d9c667d7bcc9b79732366cfd
Yogesh K U <yogesh.ku@gaiansolutions.com>
2023-12-21 18:26:54 +0530

    investmentdata-updated

--- src/molecules/investmentData/investmentData.js
+++ src/molecules/investmentData/investmentData.js
@@ -2,31 +2,31 @@ export const InvestmentData = {
   //page2
   investmentOne: [
     {
-      investors1: "17,366",
-      investorsName1: "Total Thematics Investors",
+      investors: "17,366",
+      investorsName: "Total Thematics Investors",
     },
     {
-      investors2: "$11.4M",
-      investorsName2: "Total Dollar amount Invested",
+      investors: "$11.4M",
+      investorsName: "Total Dollar amount Invested",
     },
     {
-      investors3: "$491",
-      investorsName3: "Avg Investment per user",
+      investors: "$491",
+      investorsName: "Avg Investment per user",
     },
   ],
   //page4
   investmentTwo: [
     {
-      investors1: "$0.29B",
-      investorsName1: "Total value of assets in Earn Wallets",
+      investors: "$0.29B",
+      investorsName: "Total value of assets in Earn Wallets",
     },
     {
-      investors2: "99K+",
-      investorsName2: "Total number of active Earn Wallets",
+      investors: "99K+",
+      investorsName: "Total number of active Earn Wallets",
     },
     {
-      investors3: "$69.02M",
-      investorsName3: "Total value earned by users in crypto",
+      investors: "$69.02M",
+      investorsName: "Total value earned by users in crypto",
     },
   ],
 };
