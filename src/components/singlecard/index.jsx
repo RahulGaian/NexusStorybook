e478f95df9f35023de5211e574ac0afef685e37a
rahul <rahul.vuppala@gaiansolutions.com>
2023-12-28 15:32:47 +0530

    fixed linux issue

--- src/components/singlecard/index.jsx
+++ src/components/singlecard/index.jsx
@@ -1,4 +1,4 @@
-// import Img from "../Image";
+import Img from "../Image";
 import style from "./index.module.css"
 const changeRoute=(Route)=>{
   window.location.href=Route
@@ -19,10 +19,10 @@ let SingleApp2 = ({image,heading,content,Route}) => {
                     {content}
               </div>
               <div className={style.contentbtns}>
-                   {/* <div onClick={()=>{changeRoute(Route)}}><span><Img className="inline-block" src="/images/icons/preview.svg" alt="Preview Icon" /></span><span>Preview</span></div>
-                   <div onClick={()=>{changeRoute(Route)}}><span><Img className="inline-block" src="/images/solutions/External.svg" alt="Preview Icon" /></span><span>Details</span> </div> */}
-                    <div onClick={()=>{changeRoute(Route)}}><span><img className="inline-block" src={require("./preview.png")} alt="Preview" /></span><span>Preview</span></div>
-                   <div onClick={()=>{changeRoute(Route)}}><span><img className="inline-block" src={require("./External.png")} alt="External" /></span><span>Details</span> </div>
+                   <div onClick={()=>{changeRoute(Route)}}><span><Img className="inline-block" src="/aidtaasImages/images/images/icons/preview.svg" alt="Preview Icon" /></span><span>Preview</span></div>
+                   <div onClick={()=>{changeRoute(Route)}}><span><Img className="inline-block" src="/aidtaasImages/images/images/solutions/External.svg" alt="Preview Icon" /></span><span>Details</span> </div>
+                    {/* <div onClick={()=>{changeRoute(Route)}}><span><img className="inline-block" src={require("./preview.png")} alt="Preview" /></span><span>Preview</span></div>
+                   <div onClick={()=>{changeRoute(Route)}}><span><img className="inline-block" src={require("./External.png")} alt="External" /></span><span>Details</span> </div> */}
               </div>
           </div>     
         </article>
