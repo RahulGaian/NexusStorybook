bd48db7c79847da5cce0e7b58476503ab848ae1e
hussainhafidh <hussainhafidh1@gmail.com>
2023-12-28 19:06:54 +0530

    added Launchpad pages

---
+++ src/Pages/launchpad/page1/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import LaunchpadBanner1 from "../../../molecules/launchpadBanner1";
+import Footer from "../../../molecules/footer";
+
+const LaunchpadPage1 = () => {
+  return (
+    <section>
+      <section>
+        <LaunchpadBanner1 />
+      </section>
+
+      <section>
+        <Footer />
+      </section>
+    </section>
+  );
+};
+
+export default LaunchpadPage1;
---
+++ src/Pages/launchpad/page2/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import SeriesBanner from "../../../molecules/seriesBanner";
+
+import Footer from "../../../molecules/footer";
+import AccordianFaq from "../../../molecules/accordianFaq";
+import { accordianData } from "../../../molecules/accordianFaq/accordianData";
+
+
+const LaunchpadPage2 = () => {
+  return (
+    <section>
+      <section>
+        <SeriesBanner />
+      </section>
+
+      <section>
+      <AccordianFaq data={accordianData.accordianFaq} />;      </section>
+
+      
+
+      <section>
+        <Footer />
+      </section>
+    </section>
+  );
+};
+
+export default LaunchpadPage2;
