3fddcfb10265fa424c6a205f4dde1b6be7f73ce4
Yogesh K U <yogesh.ku@gaiansolutions.com>
2024-01-03 18:11:29 +0530

    ces-page-updated

---
+++ src/Pages/ces/index.jsx
@@ -0,0 +1,41 @@
+import style from "./index.module.css";
+import React from "react";
+import CesBanner from "../../molecules/amzBanner/index";
+import HeadAndText from "../../molecules/amzHead&Text";
+import { secondHeadingData } from "../../molecules/amzHead&Text/data";
+import WhereToFind from "../../molecules/whereToFind";
+import Communities from "../../molecules/amzCommunities";
+import { communitiesData } from "../../molecules/amzCommunities/data";
+import StartBuilding from "../../molecules/amzStartBuilding";
+import Blobs from "../../molecules/blobs/index";
+
+function CesPage() {
+  return (
+    <section>
+      <Blobs top={"140%"} color="yellow" left={"-220px"}></Blobs>
+      <Blobs top={"60%"} color="yellow" left={"80%"}></Blobs>
+      <section>
+        <CesBanner />
+      </section>
+      <section>
+        <HeadAndText data={secondHeadingData.secondHeading} />
+      </section>
+      <section className={style.whereToFind}>
+        <div className={style.where}>
+          <WhereToFind />
+        </div>
+      </section>
+      <section>
+        <HeadAndText data={secondHeadingData.secondHeading2} />
+      </section>
+      <section>
+        <Communities data={communitiesData.community1} />
+      </section>
+      <section>
+        <StartBuilding />
+      </section>
+    </section>
+  );
+}
+
+export default CesPage;
---
+++ src/molecules/whereToFind/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import style from "./index.module.css";
+const WhereToFind = () => {
+  return (
+    <div className={style.main}>
+      <div className={style.internal}>
+        <h1 className={style.heading}>
+          Sign up to receive exclusive CES insights
+        </h1>
+        <h1 className={style.subheading}>
+          Get updates on the future of ambient computing and AI directly in your
+          inbox
+        </h1>
+        <div className={style.inputs}>
+          <input type="email" className={style.input1} placeholder="Email" />
+          <br />
+          <select
+            id="cars"
+            name="cars"
+            className={style.input1 + " " + style.input2}
+          >
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="fiat">Fiat</option>
+            <option value="audi">Audi</option>
+          </select>
+        </div>
+        <div className={style.checkboxx}>
+          <input
+            type="checkbox"
+            id="check"
+            name="check"
+            className={style.check}
+            value="Bike"
+          />
+          <label for="check" className={style.checkdesc}>
+            Yes, I’d like to sign-up to receive updates from Amazon Alexa about
+            new products and related services.
+          </label>
+        </div>
+        <input type="button" value={"Login"} className={style.button} />
+      </div>
+    </div>
+  );
+};
+
+export default WhereToFind;
