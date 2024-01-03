61e188f90a27ef692b6bdd1d4f0fd046af99d277
Yogesh K U <yogesh.ku@gaiansolutions.com>
2024-01-03 16:41:53 +0530

    landingpage-and-startbuilding-components-updated

---
+++ src/molecules/amzStartBuilding/index.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import StartBuilding from "./index";
+
+export default {
+  title: "components/StartBuilding",
+  component: StartBuilding,
+};
+
+export const StartBuildingComponent = () => <StartBuilding />;
