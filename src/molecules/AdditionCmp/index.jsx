3ebdec9bb00f488ddef9bd7d91d3442b4cf76c76
hussainhafidh <hussainhafidh1@gmail.com>
2024-01-05 14:57:05 +0530

    built Addition component

--- src/molecules/AdditionCmp/index.jsx
+++ src/molecules/AdditionCmp/index.jsx
@@ -1,3 +1,73 @@
+// import React from "react";
+// import style from "./index.module.css";
+
+// const AdditionCmp = () => {
+//   const fixed = 72;
+//   const metered = 2;
+//   const ad = 14;
+//   const subscription = 16;
+//   const data = 1;
+//   const total = fixed + metered + ad + subscription + data;
+
+//   return (
+//     <div>
+//       <div className={style.container}>
+//         <div className={style.contentContainer}>
+//           <div className={style.title}>
+//             <h1>A business model with the right mix of CapEx and OpEx</h1>
+//           </div>
+
+//           <div className={style.mainContent}>
+//             <div className={style.item}>
+//               <p className={style.textStyling}>Fixed</p>
+
+//               <div className={style.digitsAndSymbol}>
+//                 <p className={style.numbers}>{`$${fixed}M`}</p>
+//                 <p className={style.symbol}>+</p>
+//               </div>
+//             </div>
+//             <div className={style.item}>
+//               <p className={style.textStyling}>Metered</p>
+
+//               <div className={style.digitsAndSymbol}>
+//                 <p className={style.numbers}>{`$${metered}M`}</p>
+//                 <p className={style.symbol}>+</p>
+//               </div>
+//             </div>
+//             <div className={style.item}>
+//               <p className={style.textStyling}>Ad Based</p>
+//               <div className={style.digitsAndSymbol}>
+//                 <p className={style.numbers}>{`$${ad}M`}</p>
+//                 <p className={style.symbol}>+</p>
+//               </div>
+//             </div>
+//             <div className={style.item}>
+//               <p className={style.textStyling}>Subscription</p>
+//               <div className={style.digitsAndSymbol}>
+//                 <p className={style.numbers}>{`$${subscription}M`}</p>
+//                 <p className={style.symbol}>+</p>
+//               </div>
+//             </div>
+//             <div className={style.item}>
+//               <p className={style.textStyling}>Data as Currency</p>
+//               <div className={style.digitsAndSymbol}>
+//                 <p className={style.numbers}>{`$${data}M`}</p>
+//                 <p className={style.symbol}>=</p>
+//               </div>
+//             </div>
+//             <div className={style.item}>
+//               <p className={style.textTotal}>Total</p>
+//               <p className={style.numbersTotal}>{`$${total}M`}</p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AdditionCmp;
+
 import React from "react";
 import style from "./index.module.css";
 
@@ -19,43 +89,48 @@ const AdditionCmp = () => {
 
           <div className={style.mainContent}>
             <div className={style.item}>
-              <p>Fixed</p>
+              {/* <div className={style.para}> */}
+                <p className={style.textStyling}>Fixed</p>
+              {/* </div> */}
+
               <div className={style.digitsAndSymbol}>
-                <p>{`$${fixed}M`}</p>
-                <p>+</p>
+                <p className={style.numbers}>{`$${fixed}M`}</p>
+                <p className={style.symbol}>+</p>
+              </div>
+            </div>
+
+            <div className={style.item}>
+              <p className={style.textStyling}>Metered</p>
+
+              <div className={style.digitsAndSymbol}>
+                <p className={style.numbers}>{`$${metered}M`}</p>
+                <p className={style.symbol}>+</p>
               </div>
             </div>
             <div className={style.item}>
-              <p>Metered</p>
+              <p className={style.textStyling}>Ad Based</p>
               <div className={style.digitsAndSymbol}>
-                <div><p>{`$${metered}M`}</p></div>
-                <div><p>+</p></div>
+                <p className={style.numbers}>{`$${ad}M`}</p>
+                <p className={style.symbol}>+</p>
               </div>
             </div>
             <div className={style.item}>
-              <p>Ad Based</p>
+              <p className={style.textStyling}>Subscription</p>
               <div className={style.digitsAndSymbol}>
-                <p>{`$${ad}M`}</p>
-                <p>+</p>
+                <p className={style.numbers}>{`$${subscription}M`}</p>
+                <p className={style.symbol}>+</p>
               </div>
             </div>
             <div className={style.item}>
-              <p>Subscription</p>
+              <p className={style.textStyling}>Data as Currency</p>
               <div className={style.digitsAndSymbol}>
-                <p>{`$${subscription}M`}</p>
-                <p>+</p>
+                <p className={style.numbers}>{`$${data}M`}</p>
+                <p className={style.symbol}>=</p>
               </div>
             </div>
             <div className={style.item}>
-              <p>Data as Currency</p>
-              <div className={style.digitsAndSymbol}>
-                <p>{`$${data}M`}</p>
-                <p>=</p>
-              </div>
-            </div>
-            <div className={style.item}>
-              <p>Total</p>
-              <p>{`$${total}M`}</p>
+              <p className={style.textTotal}>Total</p>
+              <p className={style.numbersTotal}>{`$${total}M`}</p>
             </div>
           </div>
         </div>
@@ -65,39 +140,3 @@ const AdditionCmp = () => {
 };
 
 export default AdditionCmp;
-
-// // index.jsx
-// import React from "react";
-// import style from "./index.module.css";
-// import { InvestmentData } from "./data";
-
-// const AdditionCmp = () => {
-
-//   return (
-//     <div>
-//       <div className={style.container}>
-//         <div className={style.contentContainer}>
-
-//           <div className={style.title}>
-//             <h1>A business model with the right mix of CapEx and OpEx</h1>
-//           </div>
-
-//           <div className={style.mainContent}>
-//             <div className={style.investContainer}>
-//               {InvestmentData.investmentOne.map((investment, index) => (
-//                 <div key={index} className={style.investContentCont}>
-//                   <p className={style.investName}>{investment.investorsName}</p>
-//                   <p className={style.investNumber}>{investment.investors}</p>
-//                   <p className={style.investName}>+</p>
-//                 </div>
-//               ))}
-
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AdditionCmp;
