ddf0764ac1f727e47c91630e018be4a1d926d4b8
ShubhamLahase <shubham.sheshrao@gaiansolutions.com>
2024-01-08 23:59:53 +0530

    charts updated

--- src/molecules/projection/linechart.js
+++ src/molecules/projection/linechart.js
@@ -15,8 +15,8 @@ const ChartContainer = styled.div`
     height: 250px;
   }
   @media screen and (max-width: 1250px) {
-    width: 230px;
-    height: 230px;
+    width: 210px;
+    height: 210px;
   }
   @media screen and (max-width: 600px) {
     width: 300px;
@@ -40,15 +40,15 @@ const ChartComponent = ({ data }) => {
       setChartData(mValues);
     }, [selectedDMA, data]);
 
-    function formatYAxisValue(value) {
+    const formatYAxisValue = (value) => {
       if (value >= 1000000) {
-        return (value / 1000000).toFixed(1) + 'M';
+        return (value / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
       } else if (value >= 1000) {
-        return (value / 1000).toFixed(1) + 'K';
+        return (value / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
       } else {
         return value;
       }
-    }
+    };
   
     const getOption = () => {
       const xAxisData = Array.from({ length: chartData.length }, (_, index) => `YEAR_${index + 1}`);
@@ -56,6 +56,15 @@ const ChartComponent = ({ data }) => {
       return {
         grid: {
           left:65, // Adjust the left margin as needed
+        },
+        title: {
+          text: "Total Revenue",
+          bottom: 0,
+          left: "center",
+          textStyle: {
+            fontSize: 14,
+            textAlign: 'center'
+          }
         },
         xAxis: {
           type: 'category',
