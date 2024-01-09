c568706e10c616b2efcb3fce917a7e7ba41d3e90
rahul <rahul.vuppala@gaiansolutions.com>
2024-01-09 16:50:30 +0530

    changes

--- src/molecules/projection/lineSeriesdata.js
+++ src/molecules/projection/lineSeriesdata.js
@@ -1,7 +1,9 @@
 import ReactECharts from 'echarts-for-react';
 import React from 'react';
 import styled from 'styled-components';
-
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useRef } from 'react';
 const ChartContainer = styled.div`
   width: 350px;
   height: 350px;
@@ -24,9 +26,95 @@ const ChartContainer = styled.div`
   }
 `;
 const ChartComponent6 = ({data}) => {
+    let chartRef = useRef();
+  
+  const [presentApps,setPresentApps] = useState([])
+  const [option,setOption] = useState({})
+  useEffect(()=>{
+    const getOption = () => {
+      const years = Array.from({ length: 10 }, (_, index) => index + 1);
+  
+      const Allapps = ['cLINK', 'GoFEMA', 'Hear,here', "Izak", "Reve(e)", "Around", "Aegis", "Museo", "ImpressIO", "AmplyFund"];
+  
+      let data_dict = [];
+      const colorPalette = ['#c23531', '#2f4554', '#61a0a8', '#d48265', '#749f83', '#ca8622', '#bda29a', '#6e7074','#91c7ae', '#fcce10'];
+  
+      for (let i of Allapps) {
+        let app_data = years.map((year) => data[0][`${i}_YEAR_${year}`]);
+  
+        if (app_data[0] !== undefined) {
+          data_dict.push({
+            name: i,
+            type: 'line',
+            stack: 'total',
+            barWidth: '30%',
+            smooth: true,
+            itemStyle: {
+              color: colorPalette[data_dict.length % colorPalette.length], // Use a cyclic color from the palette
+  
+              opacity: 0.7
+            },
+            data: app_data,
+          });
+          // presentApps.push(i);
+          setPresentApps(prev => [...prev,i])
+        }
+      }
+  
+      return {
+        legend: {
+          data: presentApps,
+          width: "100%",
+          textStyle: {
+            fontSize: 7
+          }
+        },
+        grid: {
+          left: 65,
+        },
+        title: {
+          text: "Yearly Projection",
+          bottom: 0,
+          left: "center",
+          textStyle: {
+            fontSize: 14,
+            textAlign: 'center'
+          }
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+          },
+        },
+        yAxis: {
+          type: 'value',
+          axisLabel: {
+            formatter: formatYAxisValue,
+          },
+        },
+        xAxis: {
+          type: 'category',
+          data: years.map((year) => `Year ${year}`),
+        },
+        series: data_dict,
+      };
+    };
 
+    setOption(getOption)
+  
+    return ()=>{
+      if(chartRef !=null){
+        try{
 
-
+          chartRef.current.dispose();
+        }
+        catch{
+          console.log("removed")
+        }
+      }
+    }
+  },[data])
   const formatYAxisValue = (value) => {
     if (value >= 1000000) {
       return (value / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
@@ -39,80 +127,12 @@ const ChartComponent6 = ({data}) => {
 
   
 
-  const getOption = () => {
-    const years = Array.from({ length: 10 }, (_, index) => index + 1);
-
-    const Allapps = ['cLINK', 'GoFEMA', 'Hear,here', "Izak", "Reve(e)", "Around", "Aegis", "Museo", "ImpressIO", "AmplyFund"];
-
-    let presentApps = [];
-    let data_dict = [];
-    const colorPalette = ['#c23531', '#2f4554', '#61a0a8', '#d48265', '#749f83', '#ca8622', '#bda29a', '#6e7074','#91c7ae', '#fcce10'];
-
-    for (let i of Allapps) {
-      let app_data = years.map((year) => data[0][`${i}_YEAR_${year}`]);
-
-      if (app_data[0] !== undefined) {
-        data_dict.push({
-          name: i,
-          type: 'line',
-          stack: 'total',
-          barWidth: '30%',
-          smooth: true,
-          itemStyle: {
-            color: colorPalette[data_dict.length % colorPalette.length], // Use a cyclic color from the palette
-
-            opacity: 0.7
-          },
-          data: app_data,
-        });
-        presentApps.push(i);
-      }
-    }
-
-    return {
-      legend: {
-        data: presentApps,
-        width: "100%",
-        textStyle: {
-          fontSize: 7
-        }
-      },
-      grid: {
-        left: 65,
-      },
-      title: {
-        text: "Yearly Projection",
-        bottom: 0,
-        left: "center",
-        textStyle: {
-          fontSize: 14,
-          textAlign: 'center'
-        }
-      },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'cross',
-        },
-      },
-      yAxis: {
-        type: 'value',
-        axisLabel: {
-          formatter: formatYAxisValue,
-        },
-      },
-      xAxis: {
-        type: 'category',
-        data: years.map((year) => `Year ${year}`),
-      },
-      series: data_dict,
-    };
-  };
 
   return (
     <ChartContainer  id="main9">
     <ReactECharts
-      option={getOption()}
+    ref={chartRef}
+      option={option}
       style={{ height: '100%', width: '100%'}}
       />
       </ChartContainer>
